fb610fdb6a4125322d2dccde60a804d27e23b910
Aaron Cervantes <soy.aaron.cervantes@gmail.com>
2023-04-17 11:08:18 -0600

    feat: Added pattern for first section

--- src/components/sections/first-section.component.tsx
+++ src/components/sections/first-section.component.tsx
@@ -3,6 +3,8 @@ import ToolbarComponent from "../toolbar.component";
 import FirstSectionContent from "../first-section-content";
 
 import heroImage from '/assets/images/image-hero.webp';
+import leftPattern from '/assets/images/bg-pattern-1.svg';
+import rightPattern from '/assets/images/bg-pattern-2.svg';
 
 export const FirstSectionComponent = () => {
   return (
@@ -11,6 +13,8 @@ export const FirstSectionComponent = () => {
         <div className="[ content--visible ]">
           <ToolbarComponent />
           <FirstSectionContent/>
+          <img className="[ pattern pattern--left ]" src={leftPattern} alt=""/>
+          <img className="[ pattern pattern--right ]" src={rightPattern} alt=""/>
         </div>
         <img
           className="[ image--hero ] [ image--hero--hidden ]"
